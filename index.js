9f56e4e3af113736cef5b1b1909130947117fe81
vinithra <vinissb23698@gmail.com>
2024-07-28 20:33:42 +0530

    added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import fs from 'fs' //file system, default package
+import { format } from 'path';
 
 const app = express()
 app.use(cors()) // which domain, port
@@ -7,14 +9,27 @@ const PORT=4000 // .env
 
 
 //API generate   // '/'-> endpoints
-app.get('/first',(req,res)=>{
-    res.status(200).json({message:"welcome to backend worldb "})
+app.get('/',(req,res)=>{
+    //res.status(200).json({message:"welcome to backend worldb "})
+    res.status(200).send(
+        `<div style="background-color:blue ; color : white">
+         <h1> using html, hello </h1>
+         </div>`
+    )
+ })
+
+app.get('/writereadfile', (req, res)=>{
+    let today = format(new Date(), 'dd-MM-yyyy-HH-mm-ss') //28-07-2024-19-44-15
+    console.log(today);
+    const filepath = `TimeStamp/${today}`
+
+    fs.writeFileSync(filepath, `${today}`,'utf8');
     
-})
+    let data = fs.readFileSync(filepath, 'utf8')
+    try {
+        res.status(200).send(data)
+    } catch (error) {
+        console.log(error);
 
-//url : http://localhost:4000   / first
-
-
-app.listen(PORT ,()=>{
-    console.log("App is listening in the port:",PORT);
-}) // port : 4000 req 
+    }
+}) 
